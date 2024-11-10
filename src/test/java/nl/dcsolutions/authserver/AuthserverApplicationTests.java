ea17565783d89e51f6560a984a240e99de48a984
Teun de Clercq <teundeclercq@hotmail.com>
2024-11-10 15:16:26 +0100

    initial push

---
+++ src/test/java/nl/dcsolutions/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dcsolutions.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
